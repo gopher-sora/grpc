7b48845be53b1075f7bbf7c90ac798ed621bb65b
HtetMyoMyint <htetmyomyint.irrahub@gmail.com>
2023-04-17 02:29:24 +0700

    init

---
+++ greet/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/gopher-sora/grpc/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	list, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatal("failed to listen on: %v\n", err)
+	}
+	log.Printf("listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err := s.Serve(list); err != nil {
+		log.Fatalf("Failed to server: %v\n", err)
+	}
+}
